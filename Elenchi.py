ed29fc8d968b7fb7f34ef37c280626b6ff713d98
Li-Yixin <li.yixin@iisgalvanimi.edu.it>
2022-10-13 10:30:26 +0000

    V1.00

---
+++ Elenchi.py
@@ -0,0 +1,13 @@
+# Uno dei 4 modi di Python utilizzati per archiviare i dati, gli altri 3 sono Tuple , Set e Dictionary
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+#Le voci dell'elenco sono ordinate, modificabili e consentono valori duplicati.
+thislist = ["2", "1", "1"]
+print(thislist)
+#Lunghezza elenco , quanti elementi ha in un elenco, utilizzare la len()
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+#Gli elementi dell'elenco possono essere di qualsiasi tipo di dati
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
---
+++ Variabili.py
@@ -0,0 +1,19 @@
+# Esempio 
+# Python non deve specificare il tipo di input
+x = 5
+y = "John"
+print(x)
+print(y)
+# Casting : per specificare il tipo di dati di una variabile
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+# type(x) per sapere il tipo di input
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+# "" e '' sono la stessa cosa
+x = "John"
+x = 'John'
+# Python fa distinzione tra maiuscole e minuscole.
