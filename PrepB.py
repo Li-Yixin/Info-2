15d2f088e6a971cecb79d179a957e333d139ef45
Li-Yixin <li.yixin@iisgalvanimi.edu.it>
2022-10-27 10:55:13 +0000

    V 1.1

---
+++ PrepB.py
@@ -0,0 +1,7 @@
+#Riempi due variabili (x e y) con dei valori. Se x è maggiore di y stampa “ciao”. Se x è minore o uguale a y stampa “arrivederci”
+x=65
+y=6
+if x > y :
+    print("ciao")
+else:
+    print("arrivederci")
