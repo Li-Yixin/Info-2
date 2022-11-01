9d36a1ef9917717ace14fed153ae78135a6b4773
Li-Yixin <li.yixin@iisgalvanimi.edu.it>
2022-11-01 21:46:56 +0000

    V 1.00

---
+++ PrepD.py
@@ -0,0 +1,10 @@
+#Rifai l’esercizio C stampando il nome della variabile con il valore maggiore
+x = 1
+y = 2
+z = 3
+if x > y and y > z:
+    print("X è il numero maggiore")
+elif y > x and x > z:
+    print("Y è il numero maggiore")
+else:
+    print("Z è il numero maggiore")
