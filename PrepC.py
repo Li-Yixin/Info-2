52e6cd14b540d4ff4e99a8c2967b6bdfac2961d2
Li-Yixin <li.yixin@iisgalvanimi.edu.it>
2022-11-01 21:42:11 +0000

    V 1.00

--- PrepC.py
+++ PrepC.py
@@ -1,6 +1,8 @@
 #Riempi tre variabili (x, y ,z) con dei valori. Se x è maggiore di y e maggiore di z stampa ‘X è il numero maggiore’ 
-x=1
-y=2
-z=3
+x = 1
+y = 2
+z = 3
 if x > y and y > z:
     print("X è il numero maggiore")
+else :
+    print("X non è il numero maggiore")
