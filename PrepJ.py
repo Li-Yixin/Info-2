3b3b6a92ab0fba3b0c2e33b093f179aeeb29317a
Li-Yixin <li.yixin@iisgalvanimi.edu.it>
2022-11-02 19:20:01 +0000

    V 1.00

--- PrepJ.py
+++ PrepJ.py
@@ -1,5 +1,12 @@
 #Riempi una lista di 10 numeri casuali (utilizza un ciclo for) e stampa solo i numeri pari
-listacasuale=[1,2,3,45,686,980,676,23,47,38]
-for x in listacasuale:
-    if x%2==0:
+#creazione della lista con 10 numeri casuali
+import random
+list=[ ]
+for x in range(10):
+    list.append(random.randrange(0,19))
+print (list)
+#Stampa numeri pari
+for x in list:
+    controllo=int(x/2)
+    if (controllo*2) == x:
         print(x)
