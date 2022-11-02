4f773a56f3e1a806770cc6d13b15df7de15f6aa6
Li-Yixin <li.yixin@iisgalvanimi.edu.it>
2022-11-02 17:52:13 +0000

    V 1.00

--- Condizioni.py
+++ Condizioni.py
@@ -15,7 +15,7 @@ if b > a:
 a = 33
 b = 200
 if b > a:
-print("b is greater than a")
+  print("b is greater than a")
 
 #Elif The elif keyword is pythons way of saying "if the previous conditions were not true, then try this condition".
 a = 33
@@ -32,7 +32,7 @@ if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
-Es 2
+#Es 2
 a = 200
 b = 33
 if b > a:
--- PrepF.py
+++ PrepF.py
@@ -2,5 +2,5 @@
 lista=[1,2,3,4,5]
 x=1
 for y in lista:
-    if y > x
+    if y > x:
         print(y)
